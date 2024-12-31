acc3a66ad7ece9be27337f478950ffdb9fe0ca1a
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2024-12-31 21:28:01 +0200

    Struct

---
+++ Demo OOP Session 2/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OOP_Session_2
+{
+    internal struct Point
+    {
+        #region Fields
+        // Attributes
+        private int x;
+        private int y;
+        #endregion
+        #region Comments
+        // Constructor : is a special Function
+        //              1. Name is the same as the class name
+        //              2. No return type
+
+
+        // in Struct, we can't have a parameterless constructor
+        // This Constructor is called when we create an object of type Point
+
+        //public Point()
+        //{
+        //    x = default;
+        //    y = default;
+        //}
+        #endregion
+
+
+
+
+        public Point()
+        {
+            x = default;
+            y = default;
+        }
+
+
+        public Point(int x, int y)    // it can't be private
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(int Number)
+        {
+            this.x = this.y = Number;
+        }
+        #region Methods
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+        #endregion
+    }
+}
--- Demo OOP Session 2/Program.cs
+++ Demo OOP Session 2/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        // Entry point of the program
         static void Main(string[] args)
         {
-          
+
+
+            Point p1;
+
+            // p1 is a variable of type Point
+            // ClR will allocate memory for p1 in the stack
+
+            p1 = new Point(10, 20);
+
+        
+            Console.WriteLine(p1);
         }
     }
 }
